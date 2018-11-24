a0048645b66a12ab7dbaaa7aab0d4a2f4ca2088a
Miłosz Jankowski <miloszjankowski11@gmail.com>
2018-11-24 23:51:46 +0100

    stop conditional, no mail spam

--- scraper.py
+++ scraper.py
@@ -3,22 +3,27 @@
 from lxml import html
 import requests
 import smtplib
+import time
+import datetime
+import random
 
 
 # setup your settings below
-login = 'your@email.com'
-password = 'YourPassword'
+login = 'your email adress'
+password = 'email password'
 msg = 'Alert!'
-receiverMail = 'receiver@mail.com'
-website = 'https://websiteadress.com/index.html'
-element = '//div[@class="class name"]/text()'
+receiverMail = 'receiver email address'
+website = 'website url'
+element = '//div[@class="class name you want to check"]/text()'
 priceLevel = 100
+notSend = True
 
 
 def scrap():
     page = requests.get(website)
     tree = html.fromstring(page.content)
     price = tree.xpath(element)[0]
+    state = True
 
     if int(price) < priceLevel:
         server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -26,10 +31,16 @@ def scrap():
         server.login(login, password)
         server.sendmail(login, receiverMail, msg)
         server.quit()
-    return
+        state = False
+
+    print(datetime.datetime.now())
+    print(price)
+    return state
 
 
-# infinite loop to continuous monitoring price on the website
-# ctrl+c to stop running script
-while True:
-    scrap()
+# loop to continuous monitoring price on the website
+# ctrl+c to stop script
+while notSend:
+    notSend = scrap()
+    sleepTime = 8 + random.randint(0, 5)
+    time.sleep(sleepTime)
